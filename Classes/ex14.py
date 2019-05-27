c92e202832400806c751b0cf891a24d04949bdf4
joao <jovitor737@gmail.com>
2019-05-27 19:27:51 -0300

    add ex14

---
+++ Classes/ex14.py
@@ -0,0 +1,33 @@
+"""
+Aprimore a classe do exercício anterior para adicionar o método aumentarSalario (porcentualDeAumento)
+que aumente o salário do funcionário em uma certa porcentagem.
+"""
+
+
+class Funcionario:
+    def __init__(self, nome, salario):
+        self.nome = nome.title()
+        self.salario = salario
+
+    def aumentar_salario(self, porcentagem):
+        self.salario += self.salario * (porcentagem / 100)
+
+    def __str__(self):
+        return """
+╔════════╤═══════════╗
+║ Nome   │ Salario   ║
+╟────────┼───────────╢
+║ {:<6.6} │ {:<9.2f} ║
+╚════════╧═══════════╝
+""".format(self.nome, self.salario)
+
+
+if __name__ == '__main__':
+    nome = input('Nome: ')
+    salario = float(input('Salário: '))
+    funcionario = Funcionario(nome, salario)
+
+    porcentagem = int(input('Aumentar o salário em: '))
+    funcionario.aumentar_salario(porcentagem)
+
+    print(funcionario)

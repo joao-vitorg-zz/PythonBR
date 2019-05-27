5f8fee456fc53d86dfa0753133e59e2c640b8bae
joao <jovitor737@gmail.com>
2019-05-27 13:56:01 -0300

    add a tester

--- Classes/ex01.py
+++ Classes/ex01.py
@@ -10,13 +10,15 @@ class Bola:
     def __init__(self, material, circulo, cor):
         self.material = material.title()
         self.circulo = circulo
-        self.cor = cor.title()
+        self._cor = cor.title()
 
-    def mostra_cor(self):
-        return self.cor
+    @property
+    def cor(self):
+        return self._cor
 
-    def troca_cor(self, cor):
-        self.cor = cor
+    @cor.setter
+    def cor(self, x):
+        self._cor = x
 
     def __str__(self):
         return """
@@ -26,3 +28,12 @@ class Bola:
 ║ {0:<8.8} │ {1:<7.2f} │ {2:<8.8} ║
 ╚══════════╧═════════╧══════════╝
 """.format(self.material, self.circulo, self.cor)
+
+
+if __name__ == '__main__':
+    material = input('Material: ')
+    circulo = float(input('Circulo: '))
+    cor = input('Cor: ')
+
+    bola = Bola(material, circulo, cor)
+    print(bola)

dadfd01fd9566b34c4cc3d09dae113e92cb876d2
joao <jovitor737@gmail.com>
2019-05-27 17:45:43 -0300

    add ex06

---
+++ Classes/ex06.py
@@ -0,0 +1,42 @@
+"""
+Classe TV: Faça um programa que simule um televisor criando-o como um objeto.
+    a. O usuário deve ser capaz de informar o número do canal e aumentar ou diminuir o volume.
+    b. Certifique-se de que o número do canal e o nível do volume permanecem dentro de faixas válidas.
+"""
+
+
+class TV:
+    def __init__(self, canal=1, volume=1):
+        self.volume = volume
+        self.canal = canal
+
+    def set_canal(self, x):
+        if 0 <= x <= 100:
+            self.canal = x
+
+    def set_voume(self, x):
+        if 0 <= x <= 100:
+            self.volume = x
+
+    def up_volume(self):
+        self.volume += 1
+
+    def down_volume(self):
+        self.volume -= 1
+
+    def __str__(self):
+        return """
+╔═══════╤════════╗
+║ Canal │ Volume ║
+╟───────┼────────╢
+║ {:<5} │ {:<6} ║
+╚═══════╧════════╝
+""".format(self.canal, self.volume)
+
+
+if __name__ == '__main__':
+    canal = int(input('Canal: '))
+    volume = int(input('Volume: '))
+
+    tv = TV(canal, volume)
+    print(tv)

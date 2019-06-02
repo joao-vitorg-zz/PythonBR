2c0328b28573ef761708b1722b18db30182af569
joao <jovitor737@gmail.com>
2019-06-01 21:08:26 -0300

    add read_entrada and write_saida

--- Arquivos/ex01/main.py
+++ Arquivos/ex01/main.py
@@ -3,14 +3,25 @@ Faça um programa que leia um arquivo texto contendo uma lista de endereços IP 
 contendo um relatório dos endereços IP válidos e inválidos.
 """
 
-valido, invalido = '[Endereços válidos:]\n', '[Endereços inválidos:]\n'
-with open('entrada.txt') as entrada:
-    for ip in entrada.read().splitlines():
-        if int(max(ip.split('.'), key=int)) <= 255:
-            valido += ip + '\n'
-        else:
-            invalido += ip + '\n'
 
-with open('saida.txt', 'w') as saida:
-    saida.write(valido + '\n')
-    saida.write(invalido)
+def read_entrada():
+    valido, invalido = '[Endereços válidos:]\n', '[Endereços inválidos:]\n'
+    with open('entrada.txt') as entrada:
+        for ip in entrada.read().splitlines():
+            if int(max(ip.split('.'), key=int)) <= 255:
+                valido += ip + '\n'
+            else:
+                invalido += ip + '\n'
+
+    return valido, invalido
+
+
+def write_saida():
+    valido, invalido = read_entrada()
+    with open('saida.txt', 'w') as saida:
+        saida.write(valido + '\n')
+        saida.write(invalido)
+
+
+if __name__ == '__main__':
+    write_saida()
